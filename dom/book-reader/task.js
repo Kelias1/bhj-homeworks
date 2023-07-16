552844b6a56f25b2e2b7d768bc14f949972bd824
Elias <kel20@inbox.ru>
2023-07-16 19:44:46 +0300

    Book reader listo (готово)

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,12 +1,10 @@
-// ИЗМЕНЕНИЕ РАЗМЕРА ШРИФТА
-
 const book = document.getElementById('book');
-const btnControl = document.querySelectorAll('.book__control');
 const fontSize = document.querySelectorAll('.font-size');
-const textColor = document.querySelectorAll('.book__control_color .color');
+const colorText = document.querySelectorAll('.book__control_color .color');
+const backgroundColor = document.querySelectorAll('.book__control_background .color');
 
-for (let control of btnControl) {
-	control.onclick = function(event) {
+for (let size of fontSize) {
+	size.onclick = function(event) {
 		event.preventDefault();
 
 		if (event.target.dataset.size === 'big') {
@@ -26,38 +24,64 @@ for (let control of btnControl) {
 	}
 }
 
-// ИЗМЕНЕНИЕ ЦВЕТА ТЕКСТА. НЕ РАБОТАЕТ
+// ИЗМЕНЕНИЕ ЦВЕТА ТЕКСТА
 
-// const book = document.getElementById('book');
-// const btnControl = document.querySelectorAll('.book__control');
-// const fontSize = document.querySelectorAll('.font-size');
-// const textColor = document.querySelectorAll('.book__control_color .color');
+for (let color of colorText) {
+	color.onclick = function(event) {
+		event.preventDefault();
 
-// for (let control of btnControl) {
-// 	control.onclick = function(event) {
-// 		event.preventDefault();
+		if (event.target.dataset.textColor === 'black') {
+			book.classList.add('book_color-black');
+			book.classList.remove('book_color-gray');
+			book.classList.remove('book_color-whitesmoke');
+		} else if (event.target.dataset.textColor === 'gray') {
+			book.classList.add('book_color-gray');
+			book.classList.remove('book_color-black');
+			book.classList.remove('book_color-whitesmoke');
+		} else if (event.target.dataset.textColor === 'whitesmoke') {
+			book.classList.add('book_color-whitesmoke');
+			book.classList.remove('book_color-black');
+			book.classList.remove('book_color-gray');
+		} else {
+			book.classList.remove('book_color-black');
+			book.classList.remove('book_color-gray');
+			book.classList.remove('book_color-whitesmoke');
+		}
 
-// 		if (event.target.dataset.color === 'black') {
-// 			book.classList.add('book_color-black');
-// 			book.classList.remove('book_color-gray');
-// 			book.classList.remove('book_color-whitesmoke');
-// 		} else if (event.target.dataset.color === 'gray') {
-// 			book.classList.add('book_color-gray');
-// 			book.classList.remove('book_color-black');
-// 			book.classList.remove('book_color-whitesmoke');
-// 		} else if (event.target.dataset.color === 'whitesmoke') {
-// 			book.classList.add('book_color-whitesmoke');
-// 			book.classList.remove('book_color-black');
-// 			book.classList.remove('book_color-gray');
-// 		} else {
-// 			book.classList.remove('book_color-black');
-// 			book.classList.remove('book_color-gray');
-// 			book.classList.remove('book_color-whitesmoke');
-// 		}
+		for (let letterColor of colorText) {
+			letterColor.classList.remove('color_active');
+		}
+		event.target.classList.add('color_active');
+	}
+}
 
-// 		for (let color of textColor) {
-// 			color.classList.remove('color_active');
-// 		}
-// 		event.target.classList.add('color_active');
-// 	}
-// }
+// ИЗМЕНЕНИЕ ЦВЕТА ФОНА
+
+for (let backControl of backgroundColor) {
+	backControl.onclick = function(event) {
+		event.preventDefault();
+
+		if (event.target.dataset.bgColor === 'black') {
+			book.classList.add('book_bg-black');
+			book.classList.remove('book_bg-gray');
+			book.classList.remove('book_bg-white');
+		} else if (event.target.dataset.bgColor === 'gray') {
+			book.classList.add('book_bg-gray');
+			book.classList.remove('book_bg-black');
+			book.classList.remove('book_bg-white');
+		} else if (event.target.dataset.bgColor === 'white') {
+			book.classList.add('book_bg-white');
+			book.classList.remove('book_bg-black');
+			book.classList.remove('book_bg-gray');
+		} else {
+			book.classList.remove('book_bg-black');
+			book.classList.remove('book_bg-gray');
+			book.classList.remove('bg_color_white');
+		}
+
+		for (let back of backgroundColor) {
+			back.classList.remove('font-size_active');
+		}
+		event.target.classList.add('color_active');
+	}
+}
