1f4ab823d7c28ce04af36e595ec035e39d587616
Elias <kel20@inbox.ru>
2023-07-25 15:00:33 +0300

    first commit

---
+++ document-structure/todo/task.js
@@ -0,0 +1,36 @@
+const form = document.getElementById('tasks__form');
+const input = document.getElementById('task__input');
+const list = document.getElementById('tasks__list');
+
+form.addEventListener('submit', taskAdd);
+list.addEventListener('click', taskRemove);
+
+function taskAdd(event) {
+	event.preventDefault();
+	const taskTitle = input.value;
+	const element = document.createElement('div');
+	element.className = 'task';
+	element.innerHTML = `<div class="task__title">${input.value}</div>
+<a href="#" class="task__remove">&times;</a>`;
+	list.appendChild(element);
+	input.value = '';
+}
+
+function taskRemove(event) {
+	event.preventDefault();
+	const removeX = event.target;
+	if (removeX.classList.contains('task__remove')) {
+		let remove = removeX.closest('.task');
+		remove.remove();
+	}
+}
+
+// РАБОТАЕТ НО НЕ ПРАВИЛЬНО:
+
+function saveTask() {
+	localStorage.setItem('tasks', list.innerHTML);
+}
+
+if (localStorage.getItem('tasks')) {
+	list.innerHTML = localStorage.getItem('tasks');
+};
